0230bf53c017fb236c19a0062b80e31af79615dc
leopold <leopoldln55@gmail.com>
2022-02-11 15:31:41 +0100

    mise à niveau de l'ancien depot

---
+++ app_viewer/src/components/Agify.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Spinner from "./spinner";
+export default {
+  name: "Agify",
+
+  methods: {
+    searchAge(name){
+
+    }
+  },
+
+  created () {
+    axios
+      .get('https://api.agify.io')
+      .then(response => {
+        this.infoBrute = response;
+        this.info = this.infoBrute.data.bpi;
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally( () => this.loading = false );
+    // méthode appelée quand le callback d'une promise est executé : resolve ou reject, cela évite de dupliquer le code dans .then & .catch
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
